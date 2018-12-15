10bd8a1cc0494bdaff8f4d5b100a0557334323a9
Dieison S. Borges <dieisoncomix@gmail.com>
2018-12-15 18:13:06 -0200

    Logo

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -33,7 +33,7 @@
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="#page-top">
-          <img src="{{ asset('logo/logo-960x960-transparent.png') }}" width="100px" alt="HOPE - Igreja Batista">
+          <img src="{{ asset('logo/logo-960x960-transparent.png') }}" width="80px" alt="HOPE - Igreja Batista">
         </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
@@ -62,7 +62,7 @@
     <header class="masthead">
       <div class="container d-flex h-100 align-items-center">
         <div class="mx-auto text-center">
-          <img src="{{ asset('logo/logo-872x872-branca-transparent.png') }}" width="200px">
+          <img src="{{ asset('logo/logo-960x960-transparent.png') }}" width="200px">
           <br><br>
           <h2 class="text-white-50 mx-auto mt-2 mb-5"><i>"Aos quais Deus quis fazer conhecer quais são as riquezas da glória deste mistério entre os gentios, que é Cristo em vós, <span style="color: #ff6666;"> esperança </span> da glória"</i></h2>
           <h2 class="text-white-50 mx-auto mt-2 mb-5"><small>Colossenses 1:27 ACF</small></h2>
