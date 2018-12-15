b33645a453979995beb37b7f9c6526b0d9b7363a
Dieison S. Borges <dieisoncomix@gmail.com>
2018-12-15 17:04:14 -0200

    Site

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,431 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Igreja Batista HOPE - Curitiba - PR - Jesus">
+    <meta name="author" content="Igreja Batista HOPE">
+
+    <title>HOPE - Igreja Batista</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('site/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="{{ asset('site/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('site/css/grayscale.min.css') }}" rel="stylesheet">
+
+
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+          <img src="{{ asset('logo/logo-960x960-transparent.png') }}" width="100px" alt="HOPE - Igreja Batista">
+        </a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#nossahistoria">Nossa História</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#noscremos">Nós cremos</a>
+            </li>            
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#doacoes">Doações</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contato">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+          <h1 class="mx-auto my-0 text-uppercase">HOPE</h1>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5">Igreja Batista</h2>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5"><i>"Aos quais Deus quis fazer conhecer quais são as riquezas da glória deste mistério entre os gentios, que é Cristo em vós, <span style="color: #ff6666;"> esperança </span> da glória"</i></h2>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5"><small>Colossenses 1:27 ACF</small></h2>
+          <a href="#about" class="btn btn-primary js-scroll-trigger">Conheça Nossa História</a>
+        </div>
+      </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="nossahistoria" class="about-section text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2 class="text-white mb-4">Respondendo ao Chamado</h2>
+            <p class="text-white-50">Tudo começou com algumas famílias apaixonadas por Jesus, em unidade de visão, respondendo ao chamado pastoral, com um coração desejoso de resgatar a identidade e as <a href="#cremos"> boas novas </a> de Jesus Cristo ativando os ministérios esquecidos na vidas dos filhos.<br><br>
+            "<i style="color: #ffeb99;">E sabemos que todas as coisas contribuem juntamente para o bem daqueles que amam a Deus, daqueles que são chamados segundo o seu propósito.</i>" <small>Romanos 8:28 ACF</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Projects Section -->
+    <section id="noscremos" class="projects-section bg-light">
+      <div class="container">
+
+        
+
+        <!-- Featured Project Row -->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+          <div class="col-xl-8 col-lg-7">
+            <img class="img-fluid mb-3 mb-lg-0" src="{{ asset('site/img/bg-cruz.jpg') }}" alt="">
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Nós cremos</h4>
+              <p class="text-black-50 mb-0">Entenda a visão geral e individual de Deus para a nossa igreja.</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project One Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="{{ asset('site/img/biblia.jpg') }}" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white"><span class="fa fa-bible"></span> Palavra de Deus</h4>
+                  <p class="mb-0 text-white-50">A igreja terá como seu norte e regra de fé, única e exclusivamente a Palavra de Deus entendida no seu contexto.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project Two Row -->
+        <div class="row justify-content-center no-gutters">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="{{ asset('site/img/liberdade.jpg') }}" alt="">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 class="text-white"><span class="fa fa-dove"></span> Liberdade no Espírito</h4>
+                  <p class="mb-0 text-white-50">A igreja terá liberdade para o agir no “Mover bíblico” do Espirito Santo e não em estranhas manifestações que tirem Jesus e sua Palavra do centro do culto.</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project One Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="{{ asset('site/img/leveza.jpg') }}" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white"><span class="fa fa-feather-alt"></span> Leveza e Simplicidade</h4>
+                  <p class="mb-0 text-white-50"> A igreja desenvolverá suas atividades ministeriais pautadas na leveza e na simplicidade do Evangelho de Cristo.</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project Two Row -->
+        <div class="row justify-content-center no-gutters">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="{{ asset('site/img/lideranca.jpg') }}" alt="">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 class="text-white"><span class="fa fa-people-carry"></span> Liderança</h4>
+                  <p class="mb-0 text-white-50"> Nenhum líder da igreja deverá impor sobre seus liderados  qualquer forma de pressão, domínio ou falta de liberdade. (O líder deve influenciar…)</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project One Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="{{ asset('site/img/ajuda.jpg') }}" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white"><span class="fa fa-hands-helping"></span> Social</h4>
+                  <p class="mb-0 text-white-50"> A igreja trabalhará para desenvolver seus relacionamentos na comunidade, através serviços que contribuirão para o desenvolvimento da mesma.</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project Two Row -->
+        <div class="row justify-content-center no-gutters">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="{{ asset('site/img/reino.jpg') }}" alt="">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 class="text-white"><span class="fa fa-church"></span> A serviço do Reino de Deus</h4>
+                  <p class="mb-0 text-white-50"> A igreja terá como grande valor, o desenvolvimento de suas  atividades e a  excelência nos seus serviços ao Reino.</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project One Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="{{ asset('site/img/familia.jpg') }}" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white"><span class="fa fa-home"></span> Família</h4>
+                  <p class="mb-0 text-white-50"> O núcleo familiar projetado por Deus, através da união de um homem, uma mulher e seus filhos, são prioridade e não devemos abrir mão do convivio familiar.</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+
+    <!-- Signup Section -->
+    <section id="doacoes" class="projects-section bg-light">
+      <div class="container">
+
+        <!-- Featured Project Row -->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">          
+
+          <div class="col-xl-6 col-lg-5">
+            <img class="img-fluid mb-3 mb-lg-0" src="{{ asset('site/img/bg-trigo.jpg') }}" alt="">
+          </div>
+
+          <div class="col-xl-1 col-lg-1">
+            <div class="featured-text text-center text-lg-left">
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-200">
+              <div class="card-body text-center">
+                <i class="fa fa-piggy-bank text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Seja Nosso Parceiro Ministerial</h4>
+                <hr class="my-4">
+                <div class="small text-black-20">Faça sua contribuição voluntária e seja abençoador de vidas.</div>
+                <br>
+                  <p class="medium text-black-50 text-left">
+                    <span class="fa fa-university"></span> 
+                    Caixa Econômica Federal
+                  </p>
+                  <p class="small text-black-50 text-left">
+                    Agência: <b>0000-0</b>
+                    <br>
+                    Conta Poupança: <b>00000000-0</b>
+                    <br>
+                    Variação/Operação: <b>013</b> 
+                    <br>
+                    CPF: <b>000.000.000-00</b>              
+                  </p>                
+              </div>
+            </div>
+          </div>
+        
+          
+
+
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+    <!-- Signup Section -->
+    <section id="contato" class="signup-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-lg-8 mx-auto text-center">
+
+            <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+            <h2 class="text-white mb-5">Saiba o que acontece na HOPE.</h2>
+
+            <form class="form-inline d-flex">
+              <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Entre com um endereço de email...">
+              <button type="submit" class="btn btn-primary mx-auto">Inscrever</button>
+            </form>
+
+          </div>
+
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fa fa-clock text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Programação</h4>
+                <hr class="my-4">
+                <div class="small text-black-20">Abaixo a nossa programação:</div>
+                <br>
+                  <p class="small text-black-50 text-left">
+                    <span class="fa fa-church"></span> Cultos aos Domingos às <b>18:00</b>
+                    <br><br>
+                    <span class="fa fa-users"></span> Pequenos Grupos às Quartas-feiras às <b>19:30</b>
+                    <br><br>
+                    <span class="fa fa-male"></span><span class="fa fa-female"></span> Força HOPE Sábado (Quinzenal) às <b>19:00</b> (Jovens)
+                    <br><br>
+                    
+                  </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    
+
+    <!-- Contact Section -->
+    <section class="contact-section bg-black">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Endereço</h4>
+                <hr class="my-4">
+                <div class="small text-black-20">Ops! Ainda não temos um templo, mas assim como Jesus nos reunimos em nossas casas:</div>
+                <br>
+                  <p class="small text-black-50 text-left">
+                    <span class="fa fa-home"></span> Rua Sgt Milano, 2170 - Bacacheri
+                    <br>
+                    <span class="fa fa-home"></span> Rua João Gualberto, 1515 - Alto da Glória
+                    <br>
+                    <span class="fa fa-home"></span> Rua Sgt Erwin, 2208, Ap 201 - Bacacheri
+                  </p>
+
+                  <p class="small text-black-50 text-center">Curitiba - PR</p>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-envelope text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Email</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">
+                  Pr. Jonas Serra <br>
+                  <a href="#">prjonas@ibhope.com.br</a>
+                  <br><br>
+                  Atendimento <br>
+                   <a href="#">contato@ibhope.com.br</a>
+                  <br><br>
+                  Secretaria <br>
+                   <a href="#">secretaria@ibhope.com.br</a>
+                  <br><br>
+                  Financeiro <br>
+                   <a href="#">financeiro@ibhope.com.br</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Telefone</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">Pr. Jonas Serra <br><phone>+55 (41) 99590-2045</phone></div>
+                <br>
+                <div class="small text-black-50">Dieison Borges (Tesouraria) <br><phone>+55 (41) 99581-4222</phone></div>
+                <br>
+                <div class="small text-black-50">Secretaria <br><phone>+55 (41) 3149-4222</phone></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="social d-flex justify-content-center">          
+          <a href="https://www.facebook.com/hopeigrejabatista/" class="mx-2" target="_blank">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.instagram.com/hopeigrejabatista/" class="mx-2" target="_blank">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-black small text-center text-white-50">
+      <div class="container">
+        Copyright &copy; Igreja Batista HOPE 2018 <br><br>
+        <img src="{{ asset('logo/logo-872x872-branca-transparent.png') }}" width="100px">
+        
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ asset('site/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('site/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{ asset('site/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{ asset('site/js/grayscale.min.js') }}"></script>
+
+  </body>
+
+</html>
