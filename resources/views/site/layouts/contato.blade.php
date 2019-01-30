63069781fd718b52d08a43813c406bf6603515c2
Dieison S. Borges <dieisoncomix@gmail.com>
2019-01-30 17:55:56 -0200

    Maps

--- resources/views/site/layouts/contato.blade.php
+++ resources/views/site/layouts/contato.blade.php
@@ -10,16 +10,13 @@
                 <i class="fas fa-map-marked-alt text-primary mb-2"></i>
                 <h4 class="text-uppercase m-0">Endereço</h4>
                 <hr class="my-4">
-                <div class="small text-black-20">Venha conhecer nossa família:</div>
-                <br>
-                  <p class="small text-black-50 text-left">
-                    <span class="fa fa-home"></span> Rua dos Xaverianos, nº 327 - Bairro Alto
-                    Próximo a trincheira Bairro Alto / Bacacheri
-                    <br><br>
-                    <span class="fa fa-envelope"></span> CEP: 82820-510
-                  </p>
+                <!--<div class="small text-black-20">Venha conhecer nossa família:</div>-->  
+                  <address class="small text-black-50 text-left">
+                    <span class="fa fa-home"></span> Rua dos Xaverianos, nº 327 - Bairro Alto,  Curitiba - PR | CEP: 82820-510
+                  </address> 
+                  
 
-                  <p class="small text-black-50 text-center">Curitiba - PR</p>
+                  <iframe style="width: 100%; height: 230px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1508.9019215098178!2d-49.22000480933286!3d-25.412657632921505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce5eb4f149f99%3A0xa3444b24f7a8d171!2sR.+dos+Xaverianos%2C+327+-+Bairro+Alto%2C+Curitiba+-+PR%2C+82820-510!5e1!3m2!1spt-BR!2sbr!4v1548877895444" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                 
               </div>
             </div>
