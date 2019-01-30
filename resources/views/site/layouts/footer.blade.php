e7ce52da3cc968ac9494c0329be9cbbe6abdd663
Dieison S. Borges <dieisoncomix@gmail.com>
2019-01-30 18:37:07 -0200

    Logo Footer

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -2,7 +2,9 @@
     <footer class="bg-black small text-center text-white-50">
       <div class="container">
         Copyright &copy; Igreja Batista HOPE 2018 <br><br>
-        <img src="{{ asset('logo/logo-branca-1200x1200.png') }}" width="100px">
+
+        <a href="http://www.ibhope.com.br" class="custom-logo-link" rel="home" itemprop="url"><img width="100" height="100" src="{{ asset('logo/logo-branca-1200x1200.png') }}" class="custom-logo" alt="IB Hope" itemprop="logo" srcset="{{ asset('logo/logo-branca-1200x1200.png') }} 250w, http://www.ibhope.com.br/noticias/wp-content/uploads/2018/12/cropped-logo-preta-1200x1200-1-150x150.png 150w" sizes="(max-width: 100px) 89vw, (max-width: 100px) 54vw, (max-width: 100px) 543px, 580px" /></a>
+        <!--<img src="{{ asset('logo/logo-branca-1200x1200.png') }}" width="100px">-->
         
       </div>
     </footer>
